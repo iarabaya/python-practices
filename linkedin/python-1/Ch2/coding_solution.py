5ee7e8c9f184bb5542c57fdbcbde2fa0757ea20b
Iara Baya <iarabayav@gmail.com>
2025-08-08 19:07:19 -0300

    feat: coding solution proposed

--- linkedin/python-1/Ch2/coding_solution.py
+++ linkedin/python-1/Ch2/coding_solution.py
@@ -2,3 +2,11 @@
 def count_numbers(which, numbers):
   if which != "even" and which != "odd":
     return -1
+  
+  result = 0
+  for num in numbers:
+    if which == "even" and num % 2 == 0:
+      result += 1
+    if which == "odd" and num % 2 != 0:
+      result += 1
+  return result
