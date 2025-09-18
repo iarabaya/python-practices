52c76444594c02958e3444677c4304d74c8b0ace
Iara Baya <iarabayav@gmail.com>
2025-09-18 01:03:14 -0300

    sorts

---
+++ linkedin/programming-foundations/3-sorting-data/merge-sort.py
@@ -0,0 +1,6 @@
+# The Merge Sort Algorithm
+# Divide-and-conquer algorithm
+# Breaks a dataset into individual pieces and merges them
+# Uses recursion to operate on datasets
+# Performs well on large sets of data
+# In general has a performance of O(n log n) time complexity (log linear)
